1ed3d41927a16a14388422be16f05a60db18353c
uzz1 <uzziebrahim@gmail.com>
2023-03-06 14:00:34 +0200

    updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import cover from "../assets/file-cover.svg";
+import Header from "../components/Header";
 
 
-
+function handleMenuClick() {
+    const popupMenu = document.getElementById('popup-menu');
+    if (popupMenu) {
+      popupMenu.style.display = popupMenu.style.display === 'block' ? 'none' : 'block';
+    }
+  }
 const Home = () => {
   return (
     <div className='page-body'>
+            <Header onMenuClick={handleMenuClick}/>
     <img width="100%" src={cover} alt="cover"/>
     </div>
   )
